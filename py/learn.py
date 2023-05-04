732eeca87856e6fabc8d5131cc37bb3d73f3eb97
FoolgryGamer <40832757+FoolgryGamer@users.noreply.github.com>
2023-05-04 17:39:44 +0800

    add_hdl_stuff

---
+++ py/learn.py
@@ -0,0 +1,15 @@
+# for i in range(96,0,-1):
+#     print("slv_reg{},".format(i),end = "")
+
+# for i in range(196,100,-1):
+#     print("slv_reg{},".format(i),end = "")
+
+# for i in range(296,200,-1):
+#     print("slv_reg{},".format(i),end = "")
+
+# for i in range(397,300,-1):
+#     print("slv_reg{},".format(i),end = "")
+
+# for i in range(401,497):
+#     a = hex(i)
+#     print("9'h{}   : reg_data_out <= z[{}+:32];".format(a[2:],(i-401)*32))
